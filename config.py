34dfc536f7096d74051e89483631086177adb44b
dmhigdon8 <147114672+dmhigdon8@users.noreply.github.com>
2025-05-05 21:25:18 -0500

    server reset + euclidean distance function added to config

--- config.py
+++ config.py
@@ -6,9 +6,13 @@ import requests
 import time
 
 # VARIABLES
-token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiTE9ORVNUQVJUSUdFUiIsInZlcnNpb24iOiJ2Mi4zLjAiLCJyZXNldF9kYXRlIjoiMjAyNS0wNC0yNyIsImlhdCI6MTc0NTc4ODU3Miwic3ViIjoiYWdlbnQtdG9rZW4ifQ.YEd-r2hMKjhbDCgTV49HEJ7i_snv5TilYUKpusGLX7uK98cu7k-dW1tlLOvv_cZnIogjE_TpxYZQ9HOTAElhxaLiod4DWpJt5vIaM7SfUW0nrrtSuWbJSAWZi3ne6ySWWa35Ap7wvRuoyUzB4GBzbpCkLUXvJpvg5B9sT5edLYiPsmqU0P_N2UGWw1aqLgy9LGIb8ohMAg6CAs-OjzTNvVEX3XCieBFP8Lc7N9LUMQd5RQLbDEmeu0RP22Uqtv7BBR0Tr8t9e1JC7twRUe7uuu6CK71vqNhTFQzWLaw61k-dQ15mPTTnHhYfT1q8Y_K-Q_r-tF65R35NZvYzD3x2H3XKpJ0baUNE32Gn_zdHiWtMxne4hRspVSvF_VeFROys9DJVBrWYKCujS-yPytuH0YIahwCWFlWd1_UJ9a-bvWEGsXLR2HwrgR3S5UyQTkiz0JjIjpLTJ_me8aNA8oW52ms19rc_-zM989TeMgO01MQh3AngFpgZuAshsV8vf9Oa87Ymz2Fqk3aPBjBW_Akq0QXWpXdaq6sZxkFEXHgo-r-q3gyM112b7Wzxfahh5KVKqpWDEU-bdo6wxCSTXPPBuX2Xi_wKftjP1ko12ptecp8iCDuKQ11y6WNk4mG8VewpBzyRHBrnw8-Etyd_0UPV5x-3nb1I-N3NHclVSdnyXnM'
+token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiTE9ORVNUQVJUSUdFUiIsInZlcnNpb24iOiJ2Mi4zLjAiLCJyZXNldF9kYXRlIjoiMjAyNS0wNS0wNCIsImlhdCI6MTc0NjQ5NTk5NCwic3ViIjoiYWdlbnQtdG9rZW4ifQ.r796OR0T3cHq56J-YxkUCb4cA_a9rJJsMUUeP0f61LAv86lcoBWQ_OtN2_oRtD1vKDqTkHi9XXl8eqIibjvX08hEKLVmLo_xP0jTrKmMOgkkWXDb5Jrlnj_sU_Atn2PxbLeV_afeHQSOFPYhaKyhDZZl-yfZtEU7FdhfByrqIHYqYGsWQ8sAQ8qNcWT5e6HwIEbgxYIvF5fVsP1-dyvr8XbEUH0Wo7yCslYRU9K8ilxKc-zumojXRbuCDAFApTL-mb2UASbuyRjc5SgMV2CJwA6AEdNwWmxI2Y4L6VAgvnM2C9US-8nkeGjDHo3A2HhTBJmw96-8VxPoaCYRBQ274qAzJRWMi_kU0hpkEez1Kd9eYj69q4NMwbOSnjS01FOXJYOu_AeseNWYzMXVrIB-I9ho8Gbmm6S5l4AvOMuMSHtW-1FzGoFeaGtERR7H5hHIZpvNF_5nH4cRxScmhR-QtrPFA2e_zeN1cqkHBiDt1VZzcXOzGzLiFr9U6ARgNx164grsuWR0Fj57Q8SK_zmK52Z-0dXOwGQLSqDxRe5iIMgHEPzAKSNRU8tXv4ft0RTtp8XiSiWpf6R66WJuWZ6PPuU2Zth0m3jFY9QraBFqDwG8gnNe6EThz2Xjr9LXoiNVsx-Vhf7K7LwoxjoSBsqeWXXPCD5sR3uTobeNFTZwVaE'
 headers = {'Authorization': 'Bearer ' + token}
 url = 'https://api.spacetraders.io/v2/'
+
+response = requests.get(url + 'my/ships', headers=headers)
+ships_data = response.json()
+print(json.dumps(ships_data, indent=4))
 
 # FUNCTIONS
 
@@ -61,6 +65,9 @@ def get_ship_info(ship):
         ship_flight_mode = ships_data['data']['nav']['flightMode']
         ship_system = ships_data['data']['nav']['systemSymbol']
         ship_waypoint = ships_data['data']['nav']['waypointSymbol']
+        ship_x = ships_data['data']['nav']['route']['destination']['x']
+        ship_y = ships_data['data']['nav']['route']['destination']['y']
+        ship_coord = (ship_x, ship_y)
         ship_cargo_capacity = ships_data['data']['cargo']['capacity']
         ship_cargo_used = ships_data['data']['cargo']['units']
         ship_cargo_free = ships_data['data']['cargo']['capacity'] - ships_data['data']['cargo']['units']
@@ -74,6 +81,9 @@ def get_ship_info(ship):
                         f"---Nav Info---\n"
                       f"Ship System: {ship_system}\n"
                       f"Ship Waypoint: {ship_waypoint}\n"
+                      f"Ship Coordinates: {ship_coord}\n"
+                      f"Ship X: {ship_x}\n"
+                      f"Ship Y: {ship_y}\n"
                         f"---Cargo Info---\n"
                       f"Ship Cargo Capacity: {ship_cargo_capacity}\n"
                       f"Ship Cargo Used: {ship_cargo_used}\n"
@@ -82,11 +92,41 @@ def get_ship_info(ship):
                       f"Ship Fuel Capacity: {ship_fuel_capacity}\n"
                       f"Ship Fuel Available: {ship_fuel_available}\n"
                       f"Ship Fuel Burned: {ship_fuel_burned}\n")   
-        return ship_symbol, ship_status, ship_flight_mode, ship_system, ship_waypoint, ship_cargo_capacity, ship_cargo_used, ship_cargo_free, ship_fuel_capacity, ship_fuel_available, ship_fuel_burned
+        return ship_symbol, ship_status, ship_flight_mode, ship_system, ship_waypoint, ship_x, ship_y, ship_coord, ship_cargo_capacity, ship_cargo_used, ship_cargo_free, ship_fuel_capacity, ship_fuel_available, ship_fuel_burned
     else:
         print(f"Error: {response.status_code}")
         print(response.text)
 
+get_ship_info("LONESTARTIGER-1")
+
+
+response = requests.get(url + 'my/ships/LONESTARTIGER-1', headers=headers)
+ships_data = response.json()
+ship_x = ships_data['data']['nav']['route']['destination']['x']
+ship_y = ships_data['data']['nav']['route']['destination']['y']
+ship_coord = (ship_x, ship_y)
+print(f"Ship Coordinates: {ship_coord}")
+print(ship_x)
+print(ship_y)
+print(json.dumps(ships_data, indent=4))
+
+
+def euclidean_distance(ship, destination_coord):
+    """
+    Calculate the Euclidean distance between the ships coordinates and the given destination coordinates.
+    :ship: name of the ship of interest, used to pass to get_ship_info to get the ship's coordinates
+    :param ship_coord: Tuple of the ship's coordinates (x, y).
+    :param destination_coord: Tuple of the destination coordinates (x, y).
+    """
+    ship_coord = get_ship_info(ship)[7]
+    print(f"Ship Coordinates: {ship_coord}")
+    print(f"Destination Coordinates: {destination_coord}")
+    # Calculate the Euclidean distance
+    print(f"Euclidean Distance: {((ship_coord[0] - destination_coord[0]) ** 2 + (ship_coord[1] - destination_coord[1]) ** 2) ** 0.5}")
+    # Return the distance
+    return ((ship_coord[0] - destination_coord[0]) ** 2 + (ship_coord[1] - destination_coord[1]) ** 2) ** 0.5
+
+#euclidean_distance('LONESTARTIGER-1', (12, -24))
 
 
 
