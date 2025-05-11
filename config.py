4dbc008801fe1edaf927cf95edfba61e843b827d
dmhigdon8 <147114672+dmhigdon8@users.noreply.github.com>
2025-05-11 10:24:08 -0500

    more function iterations

--- config.py
+++ config.py
@@ -10,41 +10,13 @@ token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiTE9ORVNUQVJUSU
 headers = {'Authorization': 'Bearer ' + token}
 url = 'https://api.spacetraders.io/v2/'
 
+
 response = requests.get(url + 'my/ships', headers=headers)
 ships_data = response.json()
 print(json.dumps(ships_data, indent=4))
 
 
 # FUNCTIONS
-
-#agent & account information
-def get_agent_info():
-    """
-    Gets the agent & account information.
-    """
-    response = requests.get(url + 'my/agent', headers=headers)
-    if response.status_code == 200:
-        json_data = response.json()
-        account_id = json_data.get("data").get("accountId")
-        account_symbol = json_data.get("data").get("symbol")
-        headquarters = json_data.get("data").get("headquarters")
-        credits = json_data.get("data").get("credits")
-        faction = json_data.get("data").get("startingFaction")
-        ship_count = json_data.get("data").get("shipCount")
-        print(f"Account ID: {account_id}\n"
-                      f"Symbol: {account_symbol}\n"
-                      f"Headquarters: {headquarters}\n"
-                      f"Credits: {credits}\n"
-                      f"Faction: {faction}\n"
-                      f"Ship Count: {ship_count}\n")
-        return account_id, account_symbol, headquarters, credits, faction, ship_count
-
-    else:
-        print(f"Error: {response.status_code}")
-        print(response.text)
-
-get_agent_info()
-
 #ship information
 def get_ship_info(ship):
     """
@@ -81,6 +53,36 @@ def get_ship_info(ship):
     else:
         print(f"Error: {response.status_code}")
         print(response.text)
+
+systemSymbol = get_ship_info("LONESTARTIGER-1")['ship_system']
+#agent & account information
+def get_agent_info():
+    """
+    Gets the agent & account information.
+    """
+    response = requests.get(url + 'my/agent', headers=headers)
+    if response.status_code == 200:
+        json_data = response.json()
+        account_id = json_data.get("data").get("accountId")
+        account_symbol = json_data.get("data").get("symbol")
+        headquarters = json_data.get("data").get("headquarters")
+        credits = json_data.get("data").get("credits")
+        faction = json_data.get("data").get("startingFaction")
+        ship_count = json_data.get("data").get("shipCount")
+        print(f"Account ID: {account_id}\n"
+                      f"Symbol: {account_symbol}\n"
+                      f"Headquarters: {headquarters}\n"
+                      f"Credits: {credits}\n"
+                      f"Faction: {faction}\n"
+                      f"Ship Count: {ship_count}\n")
+        return account_id, account_symbol, headquarters, credits, faction, ship_count
+
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+
+get_agent_info()
+
 
 def print_ship_info(ship):
     """
@@ -155,7 +157,7 @@ def dock_ship(ship, destination=None):
     ship = ship.upper()   
     dock = requests.post(url + 'my/ships/' + ship + '/dock', headers=headers,
                          data={'waypointSymbol': destination})
-    return print(f"Ship status: {get_ship_info(ship)['ship_status']}.\n")
+    return f"Ship status: {get_ship_info(ship)['ship_status']}.\n"
 
 print(dock_ship('LONESTARTIGER-1'))
 
@@ -166,10 +168,14 @@ def refuel_ship(ship):    # refueling ship
     fuel_pretty = json.loads(fuel.text)
     if fuel.status_code != 200:
         error = fuel.json()
-        print(error)
-        print(f"Error: " + error['message'])
-   
-        #result = "Error: {fuel.message}"
+        if error['error']['code'] == 'INSUFFICIENT_FUNDS':
+            result = "Error: Insufficient funds to refuel."
+        elif error['error']['code'] == 'NOT_ENOUGH_FUEL':
+            result = "Error: Not enough fuel to refuel."
+        elif error['error']['code'] == 'NOT_IN_ORBIT':
+            result = "Error: Ship is not in orbit."
+        else:
+            result = error['error']['message']
     else:
         result = (f"   Refueling successful at {get_ship_info(ship)['ship_waypoint']}.\n"
                  f"    Units of fuel purchased: {fuel_pretty['data']['transaction']['units']}\n"
@@ -184,15 +190,101 @@ def launch_to_orbit(ship):  # back to orbit for travel
     print(f"Undocking ship from {get_ship_info(ship)['ship_waypoint']}.\n")
     orbit = requests.post('https://api.spacetraders.io/v2/my/ships/' + get_ship_info(ship)['ship_symbol'] + '/orbit', headers=headers)
     if orbit.status_code != 200:
-        result = "Error: {orbit.status_code}"
+        error = orbit.json()
+        result = error['error']['message']
     else:
         result = (f"    Ship status: {get_ship_info(ship)['ship_status']}\n"
                   f"    Ship waypoint: {get_ship_info(ship)['ship_waypoint']}\n"
-                  f"    Ship coordinates: {get_ship_info(ship)['ship_coord']}\n"
+                  f"    Ship coordinates: {get_ship_info(ship)['ship_coord']}\n\n"
                   f"Successfully launched to orbit.\n")
     return result
 
 print(launch_to_orbit('LONESTARTIGER-1'))
+
+
+def get_fuel_stations(systemSymbol):
+    """
+    Gets all of the available places to refuel in the system.
+    To do this, it first goes though all of the market places and checks if they have fuel as a trade good.
+    Then it goes through all of the fuel stations and adds them to the list.
+    Finally, it combines the two lists and returns them.
+    """
+    #begin by getting marketplaces with fuel as a trade good
+    response = requests.get(url + 'systems/' + systemSymbol + '/waypoints', headers=headers)
+    #print(response.text)
+    json_data = response.json()
+    waypoints = json_data.get("data", [])
+    #print(json.dumps(waypoints, indent=4))
+
+    fuel_marketplaces = []
+    for waypoint in waypoints:
+        for trait in waypoint.get("traits", []):       
+            if trait.get("symbol") == "MARKETPLACE":
+                trade_goods_response = requests.get(f'https://api.spacetraders.io/v2/systems/{systemSymbol}/waypoints/{waypoint.get("symbol")}/market', headers=headers)
+                trade_goods_json = trade_goods_response.json()
+                trade_goods = trade_goods_json.get("data", {}).get("exchange", [])
+                for trade_good in trade_goods:
+                    if trade_good['symbol'] == "FUEL":
+                        fuel_waypoint_data = {
+                            "symbol": waypoint.get("symbol"),
+                            "x": waypoint.get("x"),
+                            "y": waypoint.get("y"),
+                            "coordinates": (waypoint.get("x"), waypoint.get("y")),
+                            "type": "MARKETPLACE"
+                        }
+                        fuel_marketplaces.append(fuel_waypoint_data)
+
+    #then get all fuel stations
+    response = requests.get('https://api.spacetraders.io/v2/systems/' + systemSymbol + '/waypoints?type=' + 'FUEL_STATION', headers=headers)
+    json_data = response.json()
+    fuel_stations = json_data.get("data", [])
+
+    fuel_station_list = []
+    for fuel_station in fuel_stations:
+        fuel_station_data = {
+            "symbol": fuel_station.get("symbol"),
+            "x": fuel_station.get("x"),
+            "y": fuel_station.get("y"),
+            "coordinates": (fuel_station.get("x"), fuel_station.get("y")),
+            "type": "FUEL_STATION"
+        }
+        fuel_station_list.append(fuel_station_data)
+
+    
+    combined_fuel_station_dict = {}
+    for fuel_station in fuel_marketplaces:
+        combined_fuel_station_dict[fuel_station['symbol']] = fuel_station
+    for fuel_station in fuel_station_list:
+        if fuel_station['symbol'] not in combined_fuel_station_dict:
+            combined_fuel_station_dict[fuel_station['symbol']] = fuel_station
+    all_system_fuel_stations = list(combined_fuel_station_dict.values())
+    return all_system_fuel_stations
+
+
+def find_nearest_fuel_station(ship): #, systemsymbol):
+    """
+    Takes a ship and a system symbol and returns the nearest fuel station.
+    """
+    all_system_fuel_stations = get_fuel_stations(get_ship_info(ship)['ship_system'])
+    ship_coord = get_ship_info(ship)['ship_coord']
+    fuel_stations = []
+    for fuel_station in all_system_fuel_stations:
+        fuel_station_coord = fuel_station['coordinates']
+        distance = (euclidean_distance(ship, fuel_station['coordinates'])) 
+        fuel_station_data = {
+            "symbol": fuel_station['symbol'],
+            "coordinates": (fuel_station['x'], fuel_station['y']),
+            "distance": distance
+        }
+        fuel_stations.append(fuel_station_data)
+        #print(f"Fuel Station: {fuel_station['symbol']}; {fuel_station['coordinates']}; Distance: {distance}")   
+    closest_station = min(fuel_stations, key=lambda x: x['distance'])
+    #print(f"Closest Station: {closest_station['symbol']}; {closest_station['coordinates']}; Distance: {closest_station['distance']}")   
+    return closest_station
+
+find_nearest_fuel_station('LONESTARTIGER-1')
+
+print(find_nearest_fuel_station('LONESTARTIGER-1')['symbol'])
 
 
 
