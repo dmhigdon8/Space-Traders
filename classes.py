8ced3b79729f16c99c6bb5ee60cf6152594bc39b
dmhigdon8 <147114672+dmhigdon8@users.noreply.github.com>
2025-05-18 21:08:48 -0500

    starting classes

---
+++ classes.py
@@ -0,0 +1,7 @@
+class ship:
+    """starting a class"""
+    def __init__(self, name, cargo_capacity, fuel_capacity):
+        self.name = name
+        self.cargo_capacity = cargo_capacity
+        self.fuel_capacity = fuel_capacity
+  
